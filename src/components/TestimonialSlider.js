09a6fc2b61799dcdb01d701fe30474626a1dcb9d
Mauro.V <remanomauro@gmail.com>
2022-10-08 16:48:26 +0200

    add testimonial section

--- src/components/TestimonialSlider.js
+++ src/components/TestimonialSlider.js
@@ -1,7 +1,51 @@
 import React from 'react';
 
+// import testimonial data
+import { testimonialData, testimonialsData } from '../data'
+
+// import swiper react components
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+// import Swiper style
+import 'swiper/css'
+import 'swiper/css/pagination'
+import '../slider.css'
+
+// import required modules
+import { Pagination } from 'swiper'
+import Testimonials from './Testimonials';
+
+
 const TestimonialSlider = () => {
-  return <div>TestimonialSlider</div>;
+  return ( 
+  <Swiper slidesPerView={'auto'} centeredSlides={true} spaceBetween={20} pagination={{
+    clickable: true,
+  }} 
+  modules={[Pagination]} 
+  className='mySwiper'
+  >
+    {testimonialsData.map((slide, index) => {
+      // destructure slide
+      const { image, message, name, web } = slide
+      // slide
+      return (
+      <SwiperSlide className='bg-white rounded-[20px] border border-accent-primary xl:max-w-[645px]
+                                      max-h-[330px] pt-[60px] px-[35px] xl:px-[70px] pb-[50px] flex items-start gap-x-[30px] shadow-xl'
+                                      key={index}
+      >
+        {/* avatar image */}
+        <img src={image} alt='avatar'/>
+        {/* text */}
+        <div>
+        <div className='text-lg text-primary font-bold'>{name}</div>
+        <div className='mb-4 font-semibold text-accent-primary'>{web}</div>
+        <div className='max-w-[340px]'>{message}</div>
+        </div>
+      </SwiperSlide>
+      )
+    })}
+  </Swiper>
+  )
 };
 
 export default TestimonialSlider;
